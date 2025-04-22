2a99bf8551c0721dc95a89f487b9f6b6c774b285
*Kim Zick (rummik) <k@9k1.us>
2025-04-22 14:30:20 -0400

    Nix, wrangler, etc

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Personal email domain, etc";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs@{ flake-parts, devenv-root, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devenv.flakeModule ];
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        devenv.shells.default = {
+          name = "9k1.us";
+
+          # https://devenv.sh/reference/options/
+          packages = [ ];
+
+          languages.javascript = {
+            enable = true;
+            corepack.enable = true;
+            yarn.enable = true;
+          };
+
+          scripts.wrangler.exec = /* sh */ ''
+            npx wrangler $@
+          '';
+
+          processes.wrangler.exec = /* sh */ ''
+            wrangler dev --live-reload
+          '';
+        };
+      };
+
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
